85d76be3cd236f919f0abc399ff7d810c19bd525
AlyHammoud <hammoud1.aly1@gmail.com>
2022-06-11 00:54:39 +0300

    Vue styling and routing

---
+++ vue/src/views/Dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+<page-component title="Dashboard">Content Goes Here</page-component>
+</template>
+
+<script setup>
+import PageComponent from '../components/PageComponent.vue';
+</script>
+
+<style></style>
---
+++ vue/src/views/Surveys.vue
@@ -0,0 +1,9 @@
+<template>
+<page-component title="Surveys">Content Goes Here</page-component>
+</template>
+
+<script setup>
+import PageComponent from '../components/PageComponent.vue';
+</script>
+
+<style></style>
